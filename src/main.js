17930f4bec9f494671b8b783a1eb84637c0c67ae
Pedro Henrique <pedrofuturama620@gmail.com>
2024-05-13 22:13:20 -0300

    Criando movimentos

--- src/main.js
+++ src/main.js
@@ -5,7 +5,9 @@ kaboom()
 loadSprite("bean", "sprites/bean.png")
 loadSprite("lightening", "sprites/lightening.png")
 
-add([
+const velocidade = 320;
+
+const bean = add([
 	pos(80, 40),
 	sprite("bean"),
 ])
@@ -20,4 +22,29 @@ const raio = add([
 raio.onUpdate(() => {
 	// .angle is a property provided by rotate() component, here we're incrementing the angle by 120 degrees per second, dt() is the time elapsed since last frame in seconds
 	raio.angle += 120 * dt()
+})
+
+onClick(() => {
+	raio.moveTo(mousePos());
+})
+
+onKeyDown("right", () => {
+	bean.move(velocidade, 0);
+})
+
+onKeyDown("left", () => {
+	bean.move(-velocidade, 0);
+})
+
+onKeyDown("down", () => {
+	bean.move(0, velocidade);
+})
+
+onKeyDown("up", () => {
+	bean.move(0, -velocidade);
+})
+
+onClick(() => {
+	addKaboom(bean.pos);
+	burp();
 })
