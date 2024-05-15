d4e67a686872ca6a94dbecd122e8b6bf55460f3f
Pedro Henrique <pedrofuturama620@gmail.com>
2024-05-14 21:46:30 -0300

    Criando as Colisoes

--- src/main.js
+++ src/main.js
@@ -4,25 +4,77 @@ kaboom()
 
 loadSprite("bean", "sprites/bean.png")
 loadSprite("lightening", "sprites/lightening.png")
+loadSprite("grass", "sprites/grass.png");
+loadSprite("ghosty", "sprites/ghosty.png");
+loadSprite("aco", "sprites/steel.png");
 
 const velocidade = 320;
 
 const bean = add([
 	pos(80, 40),
 	sprite("bean"),
+	area(),
+	body(),
 ])
+
+const a = add([
+	sprite("grass"),
+	body({isStatic: true}),
+	area(),
+	pos(center()),
+	"grass",
+])
+
+const b = add([
+	sprite("aco"),
+	body({mass: 10}),
+	area(),
+	pos(100, 100),
+	"grass",
+])
+
+for(var i = 0; i < 5; i++){
+
+	const x = rand(0, width());
+	const y = rand(0, height());
+
+	const enemy = add([
+		sprite("ghosty"),
+		area(),
+		pos(x, y),
+		"enemy"
+	])
+
+}
+
+
+bean.onUpdate(() => {
+	// .isHovering() is provided by area() component, which returns a boolean of if the object is currently being hovered on
+	if (bean.isHovering()) {
+		bean.color = rgb(0, 0, 255)
+	} else {
+		bean.color = rgb()
+	}
+})
+
+bean.onCollide("enemy", (enemy) =>{
+	enemy.destroy(enemy);
+})
 
 const raio = add([
 	sprite("lightening"),
 	pos(200, 200),
 	rotate(0),
-	
+	"raio"
 ])
 
+/*
 raio.onUpdate(() => {
 	// .angle is a property provided by rotate() component, here we're incrementing the angle by 120 degrees per second, dt() is the time elapsed since last frame in seconds
 	raio.angle += 120 * dt()
 })
+*/
+
 
 onClick(() => {
 	raio.moveTo(mousePos());
@@ -45,6 +97,8 @@ onKeyDown("up", () => {
 })
 
 onClick(() => {
-	addKaboom(bean.pos);
+	addKaboom(raio.pos);
 	burp();
 })
+
+//debug.inspect = true;
