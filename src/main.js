bb95d39daa56ae26522ee078a3ce1ccde43e66b9
Pedro Henrique <pedrofuturama620@gmail.com>
2024-06-13 20:55:50 -0300

    Praticando os levels

--- src/main.js
+++ src/main.js
@@ -2,78 +2,78 @@ import kaboom from "kaboom"
 
 kaboom()
 
-loadSprite("bean", "/sprites/bean.png")
-loadSprite("spike", "/sprites/spike.png")
-loadSprite("coin", "/sprites/coin.png")
-loadSprite("grass", "/sprites/grass.png")
-loadSound("bell", "/sounds/bell.mp3")
-
-const SPEED = 500;
+loadSprite("bean", "/sprites/bean.png");
+loadSprite("grass", "/sprites/grass.png");
+loadSprite("spike", "/sprites/spike.png");
+loadSprite("coin", "/sprites/coin.png");
+loadSound("bell", "/sounds/bell.mp3");
 
 setGravity(2400)
 
+const SPEED = 500;
+
 const level = addLevel([
 	"$$$$$$$$$",
-	"@  ^   $$",
+	"@    :   ",
 	"=========",
-],  {
+], {
 	tileWidth: 64,
 	tileHeight: 64,
 
-	pos: vec2(400, 200),
+	pos: vec2(200, 200),
 
 	tiles: {
 		"@": () => [
 			sprite("bean"),
+			anchor("bot"),
 			area(),
 			body(),
-			anchor("bot"),
-			"player",
-		],
-
-		"=": () => [
-			sprite("grass"),
-			area(),
-			body({isStatic: true}),
-			anchor("bot"),
-			"chao",
+			"jogador",
 		],
 
 		"$": () => [
 			sprite("coin"),
-			area(),
 			anchor("bot"),
+			area(),
 			"moeda",
 		],
 
-		"^": () => [
+		":": () => [
 			sprite("spike"),
-			area(),
 			body({isStatic: true}),
 			anchor("bot"),
+			area(),
 			"espinho",
-		]
+		],
+
+		"=": () => [
+			sprite("grass"),
+			body({isStatic: true}),
+			anchor("bot"),
+			area(),
+			"grass",
+		],		
 	}
 })
 
-const player = level.get("player")[0]
+const player = level.get("jogador")[0];
 
 onKeyDown("space", () => {
 	if(player.isGrounded()){
-		player.jump(800)
+		player.jump();
 	}
 })
 
-onKeyDown("left", () => {
-	player.move(-SPEED, 0)
+onKeyDown("right", () => {
+	player.move(SPEED, 0);
 })
 
-onKeyDown("right", () => {
-	player.move(SPEED, 0)
+onKeyDown("left", () => {
+	player.move(-SPEED, 0);
 })
 
 player.onCollide("moeda", (moeda) => {
-	destroy(moeda);
+	moeda.destroy();
 	play("bell");
 })
 
