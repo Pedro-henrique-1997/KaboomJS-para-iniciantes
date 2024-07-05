6c8fcbdfc1f15a3cdacd27276c169b51b3ca16aa
Pedro Henrique <pedrofuturama620@gmail.com>
2024-07-05 16:52:11 -0300

    Pratica 2 da aula TIMER

--- src/main.js
+++ src/main.js
@@ -5,13 +5,12 @@ kaboom()
 loadSprite("bean", "/sprites/bean.png")
 
 loop(0.5, () => {
-
-	const player = add([
+	const jogador = add([
 		sprite("bean"),
 		pos(rand(vec2(0), vec2(width(), height())))
 	])
 
 	wait(3, () => {
-		destroy(player)
+		destroy(jogador)
 	})
 })
