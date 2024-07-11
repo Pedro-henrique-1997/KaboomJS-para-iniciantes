855647cdeb78fcd8191ebebbbe11ed6f4239619d
Pedro Henrique <pedrofuturama620@gmail.com>
2024-07-11 20:00:41 -0300

    Aula do COMPONENTS

--- src/main.js
+++ src/main.js
@@ -1,16 +1,79 @@
 import kaboom from "kaboom"
-// Start game
+
 kaboom()
 
 loadSprite("bean", "/sprites/bean.png")
 
-loop(0.5, () => {
-	const jogador = add([
-		sprite("bean"),
-		pos(rand(vec2(0), vec2(width(), height())))
-	])
+function funky(){
 
-	wait(3, () => {
-		destroy(jogador)
-	})
+	let isFunky = false;
+
+	return {
+		
+		id: "funky",
+
+		require: ["scale", "color"],
+
+		add(){
+
+		},
+
+		update(){
+
+			if(!isFunky) return
+			this.color = rgb(rand(0, 255), rand(0, 255), rand(0, 255)),
+			this.scale = rand(1, 2)
+
+		},
+
+		drawn(){
+
+		},
+
+		destroy(){
+
+		},
+
+		inspect(){
+           return isFunky ? "one" : "off";
+		},
+
+		getFunky(){
+			isFunky = true;
+		}
+
+	}
+}
+
+const bean = add([
+	sprite("bean"),
+	scale(1),
+	pos(center()),
+	anchor("center"),
+	color(),
+	area(),
+	funky(),
+	"friend",
+	{
+		coolness: 100,
+    }
+])
+
+onKeyPress("space", () => {
+	if(bean.coolness >= 100){
+		bean.getFunky()
+	}
 })
+
+onKeyPress("r", () => {
+	bean.use(rotate(rand(0, 360)))
+})
+
+onKeyPress("a", () => {
+	bean.unuse("funky")
+})
+
+add([
+	text("Aperte SPACE para começar"),
+	pos(12, 12),
+])
