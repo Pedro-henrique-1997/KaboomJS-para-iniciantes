f40a45940e0b63356ab7a6c86ab76e8f637cda6e
Pedro Henrique <pedrofuturama620@gmail.com>
2024-08-09 20:40:08 -0300

    Pratica 2

--- src/main.js
+++ src/main.js
@@ -10,35 +10,37 @@ loadSprite("grass", "/sprites/grass.png")
 loadSprite("ghosty", "/sprites/ghosty.png")
 loadSprite("portal", "/sprites/portal.png")
 loadSound("bell", "/sounds/bell.mp3")
-loadSound("cano", "/sounds/mario-entrando-no-cano.mp3")
-loadSound("up", "/sounds/levelupL.mp3")
+loadSound("cano", "/sounds/cano.mp3")
+loadSound("up", "/sounds/levelup.mp3")
 
 
 setGravity(2400)
 
+const SPEED = 480
+
 const LEVELS = [
 	[
-		"@  > $$$$$$  #",
-		"==============",
+		"@ > $$$$$$ #",
+		"============",
 	],
 
 	[
-		"@  $  $  #",
-		"=  =  =  =",
+		"@ > $ $ $ $ #",
+		"====== = = ===",
 	],
 
 	[
-		"@ > $$$$ > #",
-		"=== = = = ==",
-	],
+		"@  $  #",
+		"=  =  ="
+	]
 ]
 
 scene("game", ({levelIdx, score}) => {
-
+     
 	const level = addLevel(LEVELS[levelIdx || 0], {
 		tileWidth: 64,
 		tileHeight: 64,
-		pos: vec2(420, 200),
+		pos: vec2(200, 200),
 
 		tiles: {
 			"@": () => [
@@ -52,16 +54,23 @@ scene("game", ({levelIdx, score}) => {
 			"=": () => [
 				sprite("grass"),
 				body({isStatic: true}),
-				anchor("bot"),
 				area(),
-				"chao",
+				anchor("bot"),
+				"grass",
 			],
-			
+
 			"$": () => [
 				sprite("coin"),
 				area(),
 				anchor("bot"),
-				"coin",
+				"moeda",
+			],
+
+			">": () => [
+				sprite("spike"),
+				area(),
+				anchor("bot"),
+				"espinho",
 			],
 
 			"#": () => [
@@ -70,23 +79,90 @@ scene("game", ({levelIdx, score}) => {
 				anchor("bot"),
 				"portal",
 			],
-
-			">": () => [
-				sprite("spike"),
-				area(),
-				anchor("bot"),
-                "spike",
-			],
 		}
+	})
+
+	const player = level.get("player")[0]
+
+	function pular(){
+		if(player.isGrounded()){
+			player.jump()
+		}
+	}
+
+	onKeyPress("space", pular)
+
+	onKeyDown("right", () => {
+		player.move(SPEED, 0)
+	})
+
+	onKeyDown("left", () => {
+		player.move(-SPEED, 0)
+	})
+
+	var scoreLabel = add([
+		text(score),
+		pos(12, 12),
+	])
+
+	player.onCollide("espinho", (espinho) => {
+		go("loose")
+	})
+
+	player.onCollide("portal", () => {
+		play("cano")
+		if(levelIdx < LEVELS.length - 1){
+			go("game", {
+				score: score,
+				levelIdx: levelIdx + 1,
+			})
+		}else{
+			go("win", {
+				score: score,
+			})
+		}
+	})
+
+	player.onCollide("moeda", (moeda) => {
+		destroy(moeda),
+		score++;
+		scoreLabel.text = score;
+		play("bell")
+	})
+
+	player.onUpdate(() => {
+		if(player.pos.y >= 480){
+		go("loose")
+		}
+	})
+
+	scene("loose", () => {
+		add([
+			text("Perdeu"),
+			pos(12, 12),
+		])
+	})
+
+	scene("win", ({score}) => {
+		play("up")
+		add([
+			text(`Voçe pegou ${score} moedas`),
+			pos(12, 12)
+		])
+
+		onKeyPress(start)
 
 	})
 
-	function start() {
-		go("game",  {
-           levelIdx: 0,
-		   score: 0,
-		})
-	}
+	
 })
+
+function start(){
+	go("game", {
+		levelIdx: 0,
+		score: 0,
+	})
+	
+}
 
 start()
