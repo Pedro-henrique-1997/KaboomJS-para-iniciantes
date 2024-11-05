88e2d7de8391afea3526d7aabfa655c1f9cf5680
Pedro Henrique <pedrofuturama620@gmail.com>
2024-11-05 17:37:06 -0300

    Parte 5 pratica de rpg

--- src/main.js
+++ src/main.js
@@ -15,25 +15,28 @@ loadSprite("bean", "/sprites/bean.png")
 loadSprite("snow", "/sprites/snow.png")
 loadSprite("peixe", "/sprites/bobo.png")
 
-scene("game", (levelIdx) => {
-	
-	const caracteres = {
+scene("main", (levelIdx) => {
+
+	const SPEED = 320
+
+	// character dialog data
+	const characters = {
 		"a": {
 			sprite: "bag",
-			msg: "Pegue a chave e passe pela porta",
+			msg: "Hi Bean! You should get that key!",
 		},
-
 		"b": {
-			sprite:"ghosty",
-			msg: "Somente com a chave que podera ir a outras fases",
+			sprite: "ghosty",
+			msg: "Who are you? You can see me??",
 		},
 
 		"v": {
 			sprite: "peixe",
-			msg: "Rapido! Pegue a chave",
+			msg: "Use a chave para acessar as dungeons",
 		}
 	}
 
+	// level layouts
 	const levels = [
 		[
 			"===|====",
@@ -114,29 +117,31 @@ scene("game", (levelIdx) => {
 		// any() is a special function that gets called everytime there's a
 		// symbole not defined above and is supposed to return what that symbol
 		// means
-		
-		wildcardTile(ch){
-			const char = caracteres[ch]
-			if(char){
+		wildcardTile(ch) {
+			const char = characters[ch]
+			if (char) {
 				return [
 					sprite(char.sprite),
-					body({isStatic: true}),
 					area(),
+					body({ isStatic: true }),
 					anchor("center"),
 					"character",
-					{msg: char.msg}
+					{ msg: char.msg },
 				]
 			}
 		},
 	})
+
+	// get the player game obj by tag
+	const player = level.get("player")[0]
 
 	function addDialog(){
 		const pad = 16
 		const h = 160
 
 		const bg = add([
-			color(0,0,0),
 			rect(width(), h),
+			color(0,0,0),
 			pos(0, height() - h),
 			z(100),
 		])
@@ -145,63 +150,62 @@ scene("game", (levelIdx) => {
 			text("", {
 				width: width()
 			}),
-			pos(0 + pad, height() - h + pad),
 			z(100),
+			pos(0 + pad, height())
 		])
 
 		bg.hidden = true
 		txt.hidden = true
-
 		return {
-			active(){
-				return !bg.hidden
-			},
-
-			say(t){
+			//3.1 Exibe o texto no parametro t
+			say(t) {
 				txt.text = t
 				bg.hidden = false
 				txt.hidden = false
 			},
-
-			dismiss(){
-				if(!this.active()){
+			//3.2 Limpa o texto e limpa o background
+			dismiss() {
+				if (!this.active()) {
 					return
 				}
-
 				txt.text = ""
 				bg.hidden = true
 				txt.hidden = true
 			},
-
-			destroy(){
-				txt.destroy()
+			//3.3 Retorna true se o diálogo estiver visível, verificando se o fundo está escondido ou não.
+			active() {
+				return !bg.hidden
+			},
+			//3.4 Remove completamente o fundo e o texto da cena, destruindo os elementos.
+			destroy() {
 				bg.destroy()
-			}
+				txt.destroy()
+			},
 		}
 	}
 
-	const player = level.get("player")[0]
-
-	const SPEED = 320
-	
-	const dialog = addDialog()
-
 	const dirs = {
-		"left": LEFT,
+		"left":LEFT,
 		"right":RIGHT,
 		"up": UP,
 		"down": DOWN
 	}
 
 	for(const dir in dirs){
-		onKeyPress(dir, () => {
-			dialog.dismiss()
-		})
-
+		
 		onKeyDown(dir, () => {
 			player.move(dirs[dir].scale(SPEED))
 		})
 	}
+
 })
 
-go("game", 0)
+scene("win", () => {
+	add([
+		text("You Win!"),
+		pos(width() / 2, height() / 2),
+		anchor("center"),
+	])
+})
+
+go("main", 0)
